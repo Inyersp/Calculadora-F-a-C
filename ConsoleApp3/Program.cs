e2bbf238cfe6d1d40b1b75263c7bd62497e828b4
Inyermansp <inyersp@gmail.com>
2022-11-24 19:51:31 -0500

    Ejercicio

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicios
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            double temperaturaF, temperaturaC;
+            Console.Write("Ingresa la temperatura en grados Fahrenheit: ");
+            temperaturaF = double.Parse(Console.ReadLine());
+            Console.WriteLine("Su temperatura en grados Celcius es: ");
+            temperaturaC = (temperaturaF - 32) * 0.5556;
+            Console.WriteLine(temperaturaC);
+
+
+        }
+    }
+}
